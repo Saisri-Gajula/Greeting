1b7d78f0a6176a0e5e2bb08992f27fd79a473413
Saisri-Gajula <140395758+Saisri-Gajula@users.noreply.github.com>
2023-09-04 17:15:44 +0530

    Saved changes.

---
+++ src/main/java/com/prodapt/Learningsspring/LearningsspringApplication.java
@@ -0,0 +1,14 @@
+package com.prodapt.Learningsspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class LearningsspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningsspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prodapt/Learningsspring/LearningsspringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prodapt.Learningsspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningsspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
